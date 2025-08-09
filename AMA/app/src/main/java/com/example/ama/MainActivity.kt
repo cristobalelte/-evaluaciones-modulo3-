73ab055a43bb2944e1c58e07a8d0289d9dc90837
FerUnity <fdosx2003@gmail.com>
2025-08-09 13:50:21 -0400

    Initial commit ui components PrimaryButton content ed1

--- AMA/app/src/main/java/com/example/ama/MainActivity.kt
+++ AMA/app/src/main/java/com/example/ama/MainActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ama.ui.components.PrimaryButton
 import com.example.ama.ui.theme.AMATheme
@@ -34,13 +29,7 @@ fun AppNavigation() {
     )
 
 }
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
