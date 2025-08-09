ae5a8495e7b83db39985e4bdc83439795936526d
cristobalelte <cristobal.elte@gmail.com>
2025-08-09 18:25:29 -0400

    WIP: catálogo + detalle + carrito

--- AMA/app/src/main/java/com/example/ama/MainActivity.kt
+++ AMA/app/src/main/java/com/example/ama/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ama.ui.components.PrimaryButton
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.ama.ui.screens.catalog.CatalogRoute
+import com.example.ama.ui.screens.catalog.CatalogViewModel
+import com.example.ama.ui.screens.detail.ProductDetailRoute
 import com.example.ama.ui.theme.AMATheme
 
 class MainActivity : ComponentActivity() {
@@ -23,18 +31,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AppNavigation() {
-    PrimaryButton(
-        text = "Hola",
-        onClick = { }
-    )
+    val navController = rememberNavController()
 
-}
+    // ÚNICA instancia compartida del ViewModel para todo el NavHost
+    val vm: CatalogViewModel = viewModel()
 
+    NavHost(
+        navController = navController,
+        startDestination = "catalog"
+    ) {
+        // Catálogo
+        composable("catalog") {
+            CatalogRoute(
+                vm = vm,
+                onViewDetail = { id ->
+                    navController.navigate("detail/$id")
+                }
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AMATheme {
-        AppNavigation()
+        // Detalle por ID
+        composable(
+            route = "detail/{id}",
+            arguments = listOf(navArgument("id") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("id") ?: return@composable
+            ProductDetailRoute(
+                vm = vm, // misma instancia compartida
+                productId = id,
+                onBack = { navController.popBackStack() }
+            )
+        }
     }
 }
